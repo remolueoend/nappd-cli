c6cf3951e4f0f6b311edb5d37928a24ca113c99c
Remo Lüönd <remo.zumsteg@outlook.com>
2015-03-06 17:19:02 +0800

    Added Restart support and bugfixes

--- test/testApp.js
+++ test/testApp.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 
+console.info('Starting server...');
+
 http.createServer(function (req, res) {
+    console.info(req.method + ' ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Server is running');
+    res.end('Server is running\n');
 }).listen(9876);
